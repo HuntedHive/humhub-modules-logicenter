8c2be828af4806a9f34d6d03591302791a58c879
Den Volkov <volkovdenya@gmail.com>
2016-05-11 11:31:34 +0300

    TC-24 - fix ordering menu bar and fix home tab

--- widgets/views/logicNotification.php
+++ widgets/views/logicNotification.php
@@ -12,3 +12,22 @@
 </script>
 
 <?php } ?>
+
+<script>
+    $(document).ready(function() {
+        var orderList = new Array('Knowledge', 'My circles', 'Live Chat');
+        var menuPanelList = $("#topbar-second ul.nav:first li:visible").clone();
+
+        $("#topbar-second ul.nav:first").empty();
+        $.each(orderList, function( index, orderItem ) {
+            $.each(menuPanelList, function (index, value) {
+                var string = value.children[0].innerText;
+                if ($.trim(string) == orderItem) {
+                    $("#topbar-second ul.nav:first").append(value);
+                }
+            });
+        });
+
+        $(".listOrderLoad").animate({opacity: 1}, 500);
+    });
+</script>
