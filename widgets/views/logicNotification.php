1351219e02d5eb77028d47e2d6058559eb5caae7
Den Volkov <volkovdenya@gmail.com>
2016-05-12 15:50:10 +0300

    TC-27 - fix ajax load

--- widgets/views/logicNotification.php
+++ widgets/views/logicNotification.php
@@ -2,7 +2,9 @@
     $cs = Yii::app()->getClientScript();
     $cs->registerScriptFile("http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js");
 
-    if(LogicEntry::getStatusHomeOfUser()) {
+    $assetPrefix = Yii::app()->assetManager->publish(Yii::getPathOfAlias("application") . '/modules_core/space/resources', true, 0, defined('YII_DEBUG'));
+
+if(LogicEntry::getStatusHomeOfUser()) {
 ?>
 
 <script>
@@ -15,7 +17,7 @@
 
 <script>
     $(document).ready(function() {
-        var orderList = new Array('Knowledge', 'My circles', 'Live Chat');
+        var orderList = new Array('Knowledge', 'Home', 'События', 'My circles', 'Live Chat', 'Каталог');
         var menuPanelList = $("#topbar-second ul.nav:first li:visible").clone();
 
         $("#topbar-second ul.nav:first").empty();
@@ -29,5 +31,9 @@
         });
 
         $(".listOrderLoad").animate({opacity: 1}, 500);
+
+        $.post('<?= $assetPrefix ?>' + '/spacechooser.js', function (data) {
+           $("body").append('<script type="text/javascript">' + data + '<\/script>');
+        });
     });
 </script>
