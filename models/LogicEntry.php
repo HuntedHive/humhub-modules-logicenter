871ea4bb2472c981ac0809d050fcdbd4e929c282
Den Volkov <volkovdenya@gmail.com>
2016-05-25 10:43:27 +0300

    TC-6 - fix other check in admin and display in second step reg

--- models/LogicEntry.php
+++ models/LogicEntry.php
@@ -4,7 +4,12 @@ class LogicEntry
 {
     public static function getDropDown($type, $name = "")
     {
-            return [$name => array_merge(CHtml::listData(ManageRegistration::model()->findAll('type='. $type . " AND `default`=". ManageRegistration::DEFAULT_ADDED ." ORDER BY updated_at DESC"), 'name', 'name', 'group'), ['other' => 'other'])];
+//        return [$name => array_merge(CHtml::listData(ManageRegistration::model()->findAll('type='. $type . " AND `default`=". ManageRegistration::DEFAULT_ADDED ." ORDER BY updated_at DESC"), 'name', 'name', 'group'), ['other' => 'other'])];
+        if(empty($name)) {
+            return array_merge(CHtml::listData(ManageRegistration::model()->findAll('type='. $type . " ORDER BY updated_at DESC"), 'name', 'name', 'group'), ['other' => 'other']);
+        } else {
+            return [$name => array_merge(CHtml::listData(ManageRegistration::model()->findAll('type='. $type . self::getQueryTypeManage($type) . " ORDER BY updated_at DESC"), 'name', 'name', 'group'), (self::getStatusTypeManage($type))?['other' => 'other']:[])];
+        }
     }
 
     public static function getDropDownDepend()
