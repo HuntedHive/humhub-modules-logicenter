b9c9d074bc5389ba0e2f645eaae8920ab93532f4
Den Volkov <volkovdenya@gmail.com>
2016-06-14 15:32:35 +0300

    TC-27 fix order tabs and position

--- widgets/views/logicNotification.php
+++ widgets/views/logicNotification.php
@@ -19,17 +19,29 @@ if(LogicEntry::getStatusHomeOfUser()) {
 
 <script>
     $(document).ready(function() {
-        var orderList = new Array('Knowledge', 'Mentor circles','Mentor circle', 'All circles', 'События', 'Live Chat', 'Каталог', 'Menu');
+        var orderList = new Array('Knowledge', 'Mentor circles', 'All circles', 'Live Chat', 'Menu');
+        arrayString = orderList.toString();
+        arrayLowerString = arrayString.toLowerCase();
+        arrayLower = arrayLowerString.split(",");
         var menuPanelList = $("#topbar-second ul.nav:first li:visible").clone();
+        var orderListOther = {};
 
         $("#topbar-second ul.nav:first").empty();
         $.each(orderList, function( index, orderItem ) {
-            $.each(menuPanelList, function (index, value) {
+            $.each(menuPanelList, function (index2, value) {
                 var string = value.children[0].innerText;
                 if ($.trim(string) == orderItem) {
                     $("#topbar-second ul.nav:first").append(value);
+                } else {
+                    if($.inArray($.trim(string).toLowerCase(), arrayLower) == -1) {
+                        orderListOther[$.trim(string).toLowerCase()] = value;
+                    }
                 }
             });
+        });
+        $("#topbar-second ul.nav:last").after("<ul class='pull-right nav-right nav'></ul>");
+        $.each(orderListOther, function (index, orderItem) {
+            $(".nav-right").append(orderItem);
         });
 
         $(".listOrderLoad").animate({opacity: 1}, 500);
