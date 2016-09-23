cbdec8daec3f202511fbc37f43762e3dfd8ecdb3
VolkovDenis <volkovdenya@gmail.com>
2016-09-23 13:16:17 +0300

    license

--- widgets/LogicNotificationWidget.php
+++ widgets/LogicNotificationWidget.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+ * @link https://www.humhub.org/
+ * @copyright Copyright (c) 2015 HumHub GmbH & Co. KG
+ * @license https://www.humhub.org/licences
+ */
+
 namespace humhub\modules\logicenter\widgets;
 
 class LogicNotificationWidget extends \humhub\components\Widget
